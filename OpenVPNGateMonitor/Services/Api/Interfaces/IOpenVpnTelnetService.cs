043946e81959a36f6bb4aa43922db0f40b488226
Ivan Kolganov <imkolganov@gmail.com>
2025-02-19 22:05:43 +0200

    added connect by server id

---
+++ OpenVPNGateMonitor/Services/Api/Interfaces/IOpenVpnTelnetService.cs
@@ -0,0 +1,8 @@
+﻿namespace OpenVPNGateMonitor.Services.Api.Interfaces;
+
+public interface IOpenVpnTelnetService
+{
+    Task HandleWebSocketAsync(HttpContext context, string ip, int port);
+    Task HandleWebSocketByServerIdAsync(HttpContext context, int openVpnServerId);
+
+}
