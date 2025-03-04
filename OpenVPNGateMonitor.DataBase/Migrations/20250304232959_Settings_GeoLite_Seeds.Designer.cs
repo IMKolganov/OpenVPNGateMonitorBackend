a61e700f53608507a903f03811f74072da164c2e
Ivan Kolganov <imkolganov@gmail.com>
2025-03-05 01:31:08 +0200

    added migration for geolite settings seeds

--- OpenVPNGateMonitor.DataBase/Migrations/20250304232959_Settings_GeoLite_Seeds.Designer.cs
+++ OpenVPNGateMonitor.DataBase/Migrations/20250304232959_Settings_GeoLite_Seeds.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using OpenVPNGateMonitor.DataBase.Contexts;
@@ -11,9 +12,11 @@ using OpenVPNGateMonitor.DataBase.Contexts;
 namespace OpenVPNGateMonitor.DataBase.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250304232959_Settings_GeoLite_Seeds")]
+    partial class Settings_GeoLite_Seeds
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ OpenVPNGateMonitor.DataBase/Migrations/20250304232959_Settings_GeoLite_Seeds.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace OpenVPNGateMonitor.DataBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class Settings_GeoLite_Seeds : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                schema: "xgb_dashopnvpn",
+                table: "Settings",
+                columns: new[] { "Id", "BoolValue", "DateTimeValue", "DoubleValue", "IntValue", "Key", "StringValue", "ValueType" },
+                values: new object[,]
+                {
+                    { 5, null, null, null, null, "GeoIp_Download_Url", "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key={LicenseKey}&suffix=tar.gz", "string" },
+                    { 6, null, null, null, null, "GeoIp_Download_Url_Type", "string", "string" },
+                    { 7, null, null, null, null, "GeoIp_Db_Path", "GeoLite2/GeoLite2-City.mmdb", "string" },
+                    { 8, null, null, null, null, "GeoIp_Db_Path_Type", "string", "string" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "xgb_dashopnvpn",
+                table: "Settings",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                schema: "xgb_dashopnvpn",
+                table: "Settings",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                schema: "xgb_dashopnvpn",
+                table: "Settings",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                schema: "xgb_dashopnvpn",
+                table: "Settings",
+                keyColumn: "Id",
+                keyValue: 8);
+        }
+    }
+}
