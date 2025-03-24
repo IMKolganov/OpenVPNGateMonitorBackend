57aeadde741ca32e9177aff0c6f71425e666ba16
Ivan Kolganov <imkolganov@gmail.com>
2025-03-24 04:10:42 +0200

    added functional for load or gen jwt token

---
+++ OpenVPNGateMonitor/Configurations/JwtSecretLoaderConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using System.Security.Cryptography;
+using ILogger = Serilog.ILogger;
+
+namespace OpenVPNGateMonitor.Configurations;
+
+public static class JwtSecretLoaderConfiguration
+{
+    private const string DockerSecretPath = "/app/secrets/jwt-secret.txt";
+    private const string LocalSecretPath = "secrets/jwt-secret.txt";
+
+    public static string LoadOrGenerateSecret(ILogger logger)
+    {
+        var isDocker = Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true";
+
+        logger.Information("DOTNET_RUNNING_IN_CONTAINER = {IsDocker}", isDocker);
+
+        var relativePath = isDocker ? DockerSecretPath : LocalSecretPath;
+        var fullPath = Path.GetFullPath(relativePath);
+        Directory.CreateDirectory(Path.GetDirectoryName(fullPath)!);
+
+        string jwtSecret;
+
+        if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("JWT_SECRET")))
+        {
+            jwtSecret = Environment.GetEnvironmentVariable("JWT_SECRET")!;
+            File.WriteAllText(fullPath, jwtSecret);
+            logger.Information("JWT secret loaded from environment and saved to file at: {Path}", fullPath);
+        }
+        else if (File.Exists(fullPath))
+        {
+            jwtSecret = File.ReadAllText(fullPath);
+            logger.Information("JWT secret loaded from file at: {Path}", fullPath);
+        }
+        else
+        {
+            jwtSecret = Convert.ToHexString(RandomNumberGenerator.GetBytes(64));
+            File.WriteAllText(fullPath, jwtSecret);
+            logger.Information("JWT secret generated and saved to file at: {Path}", fullPath);
+        }
+
+        return jwtSecret;
+    }
+}
--- OpenVPNGateMonitor/Program.cs
+++ OpenVPNGateMonitor/Program.cs
@@ -1,6 +1,15 @@
 using OpenVPNGateMonitor.Configurations;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.ConfigureSerilog(builder.Configuration);
+
+// 🔐
+var logger = Log.ForContext("SourceContext", "JwtSecretLoader");
+
+var jwtSecret = JwtSecretLoaderConfiguration.LoadOrGenerateSecret(logger);
+builder.Configuration["Jwt:Secret"] = jwtSecret;
 
 builder.Services.ConfigureServices(builder.Configuration);
 builder.Services.ConfigureGeoLiteServices();
@@ -9,10 +18,7 @@ builder.Services.DataBaseServices(builder.Configuration);
 builder.Services.ConfigureJwt(builder.Configuration);
 builder.Services.ConfigureMapster();
 
-builder.Host.ConfigureSerilog(builder.Configuration);
-
 builder.ConfigureWebHost();
-
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
