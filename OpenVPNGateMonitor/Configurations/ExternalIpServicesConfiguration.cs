1a82ce3c6dc4f1c1d25cc0d605f7ceb1e3711202
Ivan Kolganov <imkolganov@gmail.com>
2025-03-25 05:03:55 +0200

    get ExternalIpServices from ExternalIpServices.json

---
+++ OpenVPNGateMonitor/Configurations/ExternalIpServicesConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace OpenVPNGateMonitor.Configurations;
+
+public static class ExternalIpServicesConfiguration
+{
+    public static void ConfigureExternalIpServices(this WebApplicationBuilder builder)
+    {
+        builder.Configuration
+            .AddJsonFile("externalipsettings.json", optional: true, reloadOnChange: true);
+    }
+}
