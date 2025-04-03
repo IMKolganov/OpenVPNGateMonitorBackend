b96c50f472742cdce496f8cddcbd2259a7767945
Иван Колганов <110064602+IMKolganov@users.noreply.github.com>
2025-04-03 19:36:04 +0300

    fixed getOvpnFileConfig

--- OpenVPNGateMonitor.SharedModels/OpenVpnServerOvpnFileConfig/Responses/OvpnFileConfigResponse.cs
+++ OpenVPNGateMonitor.SharedModels/OpenVpnServerOvpnFileConfig/Responses/OvpnFileConfigResponse.cs
@@ -2,6 +2,7 @@
 
 public class OvpnFileConfigResponse
 {
+    public int Id { get; set; }
     public int VpnServerId { get; set; }
     public string VpnServerIp { get; set; } = string.Empty;
     public int VpnServerPort { get; set; }
